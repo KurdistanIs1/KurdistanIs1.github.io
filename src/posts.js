a4a0aa9642e18c7c686175ab10d69824337e261c
KurdistanIs1 <98112941+KurdistanIs1@users.noreply.github.com>
2022-02-15 19:46:19 +0100

    Update posts.js

--- src/posts.js
+++ src/posts.js
@@ -1,46 +1,16 @@
 document.getElementById("post").innerHTML = `<div align="center" class="blog-posts hfeed container">
-  
-  <article lass="hentry">
-    <a href="#"><div class="post-contaniner">
-      <div class="post-image">
-        <img 
-        src="src/posts-img/1.png"
-        >
-      </div>
-      <div class="post-content">
-        <h5>TermuxSetting</h5>
-      </div>
-    </div></a>
-  </article>
-  
-  <!--
 
-  <article lass="hentry">
-    <div class="post-contaniner">
-      <div class="post-image">
-        <a href="#"><img 
-        src="https://e.top4top.io/p_1849vjc6s0.jpg"
-        ></a>
-      </div>
-      <div class="post-content">
-        <h5>title</h5>
-      </div>
+<div class="post-contaniner">
+  <a href="#">
+    <div class="post-image">
+      <img 
+        src="posts-img/1.png"
+        >
     </div>
-  </article>
-  
-  <article lass="hentry">
-    <div class="post-contaniner">
-      <div class="post-image">
-        <a href="#"><img 
-        src="https://e.top4top.io/p_1849vjc6s0.jpg"
-        ></a>
-      </div>
-      <div class="post-content">
-        <h5>title</h5>
-      </div>
+    <div class="post-content">
+      <h5>TermuxSetting</h5>
     </div>
-  </article>
-  
-  -->
-  
+  </a>
+</div>
+
 </div>`;
