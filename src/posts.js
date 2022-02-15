50352822a263a05a7f02c252a77edc7bcfd5e613
KurdistanIs1 <98112941+KurdistanIs1@users.noreply.github.com>
2022-02-15 20:06:23 +0100

    Update posts.js

--- src/posts.js
+++ src/posts.js
@@ -1,14 +1,16 @@
 document.getElementById("post").innerHTML = `<div align="center" class="blog-posts hfeed container">
 
-<div class="post-contaniner">
-  <a href="#">
-    <div class="post-image">
-      <img src="https://raw.githubusercontent.com/KurdistanIs1/KurdistanIs1.github.io/main/src/posts-img/1.png" title="TermuxSetting" alt="TermuxSetting" />
-    </div>
-    <div class="post-content">
-      <h5>TermuxSetting</h5>
-    </div>
-  </a>
-</div>
+<article lass="hentry">
+  <div class="post-contaniner">
+    <a href="#">
+      <div class="post-image">
+        <img src="https://raw.githubusercontent.com/KurdistanIs1/KurdistanIs1.github.io/main/src/posts-img/1.png" />
+      </div>
+      <div class="post-content">
+        <h5>TermuxSetting</h5>
+      </div>
+    </a>
+  </div>
+</article>
 
 </div>`;
